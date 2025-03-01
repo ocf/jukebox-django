6f80d684f3fe01258f66064d0801017d90ca74db
Ronit Nath <ronitnath@berkeley.edu>
2025-03-01 14:53:38 -0800

    Update shell.nix to use virtual environment

--- shell.nix
+++ shell.nix
@@ -6,7 +6,8 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     python311
-    python311Packages.poetry
+    python311Packages.pip
+    python311Packages.poetry-core
     portaudio
   ];
 
@@ -17,8 +18,19 @@ pkgs.mkShell {
     # Use the portaudio library
     export LD_LIBRARY_PATH=${pkgs.portaudio}/lib:$LD_LIBRARY_PATH
     
+    # Create a local virtual environment for pip packages
+    if [ ! -d .venv ]; then
+      echo "Creating virtual environment..."
+      ${pkgs.python311}/bin/python -m venv .venv
+      . .venv/bin/activate
+      pip install django-icons==24.4
+    else
+      . .venv/bin/activate
+    fi
+    
     # Note for users
-    echo "Nix development environment for jukebox-django"
+    echo "Nix development environment for jukebox-django activated!"
+    echo "Virtual environment is active with all dependencies installed."
     echo "To run the backend server: cd jukebox/backend && python runner.py"
     echo "To run the Django server: cd jukebox && python manage.py runserver"
   '';
