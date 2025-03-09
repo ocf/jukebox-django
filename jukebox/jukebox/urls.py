7a102fc2ef4259ed6a6186f186b215f3dc1bf2b3
Ronit Nath <ronitnath@berkeley.edu>
2025-03-08 16:53:54 -0800

    redirect 404 to dashboard default

--- jukebox/jukebox/urls.py
+++ jukebox/jukebox/urls.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import handler404
+from django.shortcuts import redirect
+
+def custom_404(request, exception):
+    return redirect('/YTUSRN')
+
+handler404 = custom_404
 
 urlpatterns = [
     path("YTUSRN/", include("YTUSRN.urls")),
