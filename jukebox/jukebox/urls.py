9f0ae7afba919edb48addbe4a3d3d00f68b9fa9a
Ronit Nath <ronitnath@berkeley.edu>
2025-03-08 17:25:43 -0800

    redirect to dashboard real

--- jukebox/jukebox/urls.py
+++ jukebox/jukebox/urls.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import handler404
 from django.shortcuts import redirect
-
-def custom_404(request, exception):
-    return redirect('/YTUSRN')
-
-handler404 = custom_404
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("YTUSRN/", include("YTUSRN.urls")),
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/YTUSRN/")),
 ]
