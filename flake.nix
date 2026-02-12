3a96c7b8b00921c59bc9c8ff62c43e2f7f24d0ca
Daniel Liu <danielliu@berkeley.edu>
2026-02-12 12:25:41 -0800

    fix error with flake

--- flake.nix
+++ flake.nix
@@ -3,8 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
-    uv2nix.url = "github:pyproject-nix/uv2nix";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+    };
   };
 
   outputs = { self, nixpkgs, pyproject-nix, uv2nix, ... }:
@@ -12,21 +19,24 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
 
-      workspace = uv2nix.lib.${system}.loadWorkspace {
-        src = ./.;
+      workspace = uv2nix.lib.workspace.loadWorkspace {
+        workspaceRoot = ./.;
       };
 
-      pythonEnv = pyproject-nix.lib.${system}.mkPythonApplication {
-        inherit (workspace) name version src;
-        pyproject = workspace.pyproject;
-        lockFile = workspace.lockFile;
-        python = pkgs.python312;
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
       };
+
+      pythonSet = (pkgs.callPackage pyproject-nix.buildPackages.pyproject-set {
+        python = pkgs.python312;
+      }).overrideScope overlay;
+
+      virtualenv = pythonSet.mkVirtualEnv "jukebox-django-dev" workspace.deps.default;
 
     in {
       devShells.${system}.default = pkgs.mkShell {
         packages = [
-          pythonEnv
+          virtualenv
           pkgs.uv
           pkgs.ffmpeg
           pkgs.portaudio
