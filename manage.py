7ab35b746dbf085cd93863c78cbf4bae8c98a854
Daniel Liu <danielliu@berkeley.edu>
2026-02-12 15:07:28 -0800

    Fix issue with directories

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
