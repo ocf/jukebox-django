a27330db909dc46f623d2ca5508e313ddf1d080e
Daniel Liu <75332824+danxliu@users.noreply.github.com>
2026-03-04 00:40:23 -0800

    Added REST API to get current player state (#23)
    Rest API (/api/state)

--- jukebox/views.py
+++ jukebox/views.py
@@ -1,5 +1,15 @@
+import jsonpickle
+from django.http import HttpResponse
 from django.shortcuts import render
+from .controller import get_controller
 
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+
+def state_api(request):
+    controller = get_controller()
+    state = controller.get_state()
+    data = jsonpickle.encode(state, unpicklable=False)
+    return HttpResponse(data, content_type="application/json")
